b7d663a211a40cc43b1c0b25c7055781efcbca6e
mferguder <50220854+mferguder@users.noreply.github.com>
2021-09-29 11:38:51 -0400

    Use old Blocking function, new Spectra_2D function
    blocking data is being carried temporarily

--- Blocking_2D.py
+++ Blocking_2D.py
@@ -11,40 +11,39 @@ def BlockFunction(x,a,b):
     for i in range(len(x)): result[i]=np.real(a*cmath.sqrt(1-b*b-2*b*(1-np.power(b+0j,np.power(2,x[i])))/np.power(2,x[i]))/(1-b))
     return result
 #----------------------------------------------------------------------
-def Blocking_2D(xqt):
-    data=xqt.transpose()
-    g=data.shape[0]
-    l=len(data[0,0])
+def Blocking(xq2t,qq):
+    l=len(xq2t[0])
     divs=divisors(l)
-    blomax=int(0.5+np.log2(l))   
-    dxq2=np.zeros((g,g))
-    bloxydys=np.zeros((g,g,3,len(divs)-2))
-    for ix in range(g):
-        for iy in range(g):
-            hi2t=data[ix,iy]
-            blox=np.log2(divs)
-            blox=blox[0:-2]
-            bloy=np.zeros(len(divs)-2)
-            dbloy=np.zeros(len(divs)-2)        
-            for j in range(len(divs)-2):
-                sz=divs[j]
-                temp=np.zeros(int(np.floor(l/float(sz))));
-                for k in range(int(np.floor(l/float(sz)))):
-                    temp[k]=np.mean( hi2t[ k*sz:(k+1)*sz ] )
-                bloy[j]=np.std(temp)/np.sqrt(len(temp)-1)
-                dbloy[j]=bloy[j]/np.sqrt(2*len(temp)-2)
-            popt, pcov = curve_fit(BlockFunction, blox, bloy, p0=[bloy[0],0.2], sigma=dbloy)
-            dxq2[ix,iy]=np.sqrt((1+popt[1])/(1-popt[1]))*bloy[0]
-            #dhq2[i]=np.std(hq2t[i])
-            bloxydys[ix,iy]=np.array([blox,bloy,dbloy])
+    blomax=int(0.5+np.log2(l))
+    dxq2=np.zeros(len(qq))
+    bloxydys=np.zeros((len(qq),3,len(divs)-2))
+    for i in range(len(qq)):
+        xi2t=xq2t[i]
+        blox=np.log2(divs)
+        blox=blox[0:-2]
+        bloy=np.zeros(len(divs)-2)
+        dbloy=np.zeros(len(divs)-2)
+        for j in range(len(divs)-2):
+            sz=divs[j]
+            temp=np.zeros(int(np.floor(l/float(sz))));
+            for k in range(int(np.floor(l/float(sz)))):
+                temp[k]=np.mean( xi2t[ k*sz:(k+1)*sz ] )
+            bloy[j]=np.std(temp)/np.sqrt(len(temp)-1)
+            dbloy[j]=bloy[j]/np.sqrt(2*len(temp)-2)
+        popt, pcov = curve_fit(BlockFunction, blox, bloy, p0=[bloy[0],0.2], sigma=dbloy)
+        dxq2[i]=np.sqrt((1+popt[1])/(1-popt[1]))*bloy[0]
+        #dxq2[i]=np.std(xq2t[i])
+        bloxydys[i]=np.array([blox,bloy,dbloy])
     return dxq2,bloxydys
 #----------------------------------------------------------------------
-def Spectra_2D(qq=Q2r,hq2t,nllq2t,nLq2t): ???
-    z1=np.mean(hq2t,axis=0)*np.power(qq,4)
-    z2=np.mean(nllq2t,axis=0)*np.power(qq,2)
+def Spectra_2D(qr,hq2t,nllq2t,nLq2t):
+    z1=np.mean(hq2t,axis=0)*qr**4
+    z2=np.mean(nllq2t,axis=0)*qr**2
     z3=np.mean(nLq2t,axis=0)
-    dz1=Blocking(hq2t.T,qq)*np.power(qq,4)
-    dz2=Blocking(nllq2t.T,qq)*np.power(qq,2)
-    dz3=Blocking(nLq2t.T,qq)
+    dz1,bloxydys = Blocking(hq2t.T,qr)
+    dz2,bloxydys = Blocking(nllq2t.T,qr)
+    dz3,bloxydys = Blocking(nLq2t.T,qr)
+    dz1*=qr**4
+    dz2*=qr**2
     return z1,z2,z3,dz1,dz2,dz3
 
